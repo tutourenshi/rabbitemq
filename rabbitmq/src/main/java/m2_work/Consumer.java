53ad7dec38cf4007e88daf1131a7c8ac7315b7d4
wang <736113959@qq.com>
2020-08-28 16:13:47 +0800

    MQ

---
+++ rabbitmq/src/main/java/m2_work/Consumer.java
@@ -0,0 +1,46 @@
+package m2_work;
+
+import com.rabbitmq.client.*;
+
+import java.io.IOException;
+
+public class Consumer {
+    public static void main(String[] args) throws Exception {
+        ConnectionFactory f = new ConnectionFactory();
+        f.setHost("192.168.64.140");
+        f.setUsername("admin");
+        f.setPassword("admin");
+        Channel c=f.newConnection().createChannel();
+        c.queueDeclare("helloworld",false,false,false,null);
+        System.out.println("等待接收数据");
+
+        //收到消息后用来处理消息的回调对象
+        DeliverCallback callback = new DeliverCallback() {
+            @Override
+            public void handle(String consumerTag, Delivery message) throws IOException {
+                String msg = new String(message.getBody(), "UTF-8");
+                System.out.println("收到: "+msg);
+
+                //遍历字符串中的字符,每个点使进程暂停一秒
+                for (int i = 0; i < msg.length(); i++) {
+                    if (msg.charAt(i)=='.') {
+                        try {
+                            Thread.sleep(1000);
+                        } catch (InterruptedException e) {
+                        }
+                    }
+                }
+                System.out.println("处理结束");
+            }
+        };
+
+        //消费者取消时的回调对象
+        CancelCallback cancel = new CancelCallback() {
+            @Override
+            public void handle(String consumerTag) throws IOException {
+            }
+        };
+
+        c.basicConsume("helloworld", true, callback, cancel);
+    }
+}
---
+++ rabbitmq/src/main/java/m2_work/Producre.java
@@ -0,0 +1,35 @@
+package m2_work;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.util.Scanner;
+
+public class Producre {
+    public static void main(String[] args)throws Exception {
+        //创建连接工厂,并设置连接信息
+        ConnectionFactory f = new ConnectionFactory();
+        f.setHost("192.168.64.140");
+        //f.setPort(5672);  //默认端口可以省略
+        f.setUsername("admin");
+        f.setPassword("admin");
+
+        Channel c=f.newConnection().createChannel();
+
+        c.queueDeclare("helloworld",false,false,false,null);
+        while (true) {
+            //控制台输入的消息发送到rabbitmq
+            System.out.print("输入消息: ");
+            String msg = new Scanner(System.in).nextLine();
+            //如果输入的是"exit"则结束生产者进程
+            if ("exit".equals(msg)) {
+                break;
+            }
+            //参数:exchage,routingKey,props,body
+            c.basicPublish("", "helloworld", null, msg.getBytes());
+            System.out.println("消息已发送: "+msg);
+        }
+
+        c.close();
+    }
+}
